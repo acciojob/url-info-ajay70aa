77f9d1de4810c6e581a385b5fb40c10ed76c1f73
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-26 19:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 const url = location.href
-const lengthOfUrl = url.length
-alert(`The length of the URL is: ${lengthOfUrl}`)
+const n = url.length
+alert(`The length of the URL is: ${n}`)
